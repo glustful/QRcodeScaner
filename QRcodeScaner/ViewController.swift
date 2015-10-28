5b7c3149cc3ac6d4cce17e5fbb3b3021b15f1d5e
guojunjun <852411097@qq.com>
2015-10-28 14:44:40 +0800

    Initial Commit

---
+++ QRcodeScaner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRcodeScaner
+//
+//  Created by yunjoy on 15/10/28.
+//  Copyright © 2015年 yunjoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
